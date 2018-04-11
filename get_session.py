2781a51b18acc582a39f46c673fe7c2aa9ae1147
Yuki Hashimoto <hashimoto@hal.t.u-tokyo.ac.jp>
2018-04-11 13:37:07 +0900

    create file

---
+++ get_session.py
@@ -0,0 +1,58 @@
+import sys
+
+import requests
+import re
+
+
+def GetLoginTokens() -> tuple:
+    COOKIE_TOKEN = "__RequestVerificationToken"
+    REGEX_HTML_TOKEN = re.compile("<input name=\"__RequestVerificationToken\" type=\"hidden\" value=\"(?P<token>.*?)\" />")
+
+    r_top = requests.get("https://lightning.bitflyer.jp/")
+    assert (r_top.status_code == 200)
+    token_html = REGEX_HTML_TOKEN.search(r_top.text).group("token")
+    token_cookie = r_top.cookies.get(COOKIE_TOKEN)
+    return token_html, token_cookie
+
+
+##############################
+
+def Login(login_id, password, token_html, token_cookie) -> tuple:
+    COOKIE_TOKEN = "__RequestVerificationToken"
+    LOGIN_ID = login_id
+    PASSWORD = password
+
+    form_data = {
+        "__RequestVerificationToken": token_html,
+        "LoginId": LOGIN_ID,
+        "Password": PASSWORD
+    }
+    # 二段階認証を適当に突破しているので，本当はここはもうちょっと丁寧にやる必要がある
+    r_auth_1 = requests.post("https://lightning.bitflyer.jp/",
+                             data=form_data,
+                             cookies={COOKIE_TOKEN: token_cookie})
+    assert r_auth_1.url == "https://lightning.bitflyer.jp/trade"
+
+    session_1 = r_auth_1.history[0].cookies.get("ASP.NET_SessionId")
+    session_2 = r_auth_1.history[1].cookies.get("api_session_v2")
+
+    return session_1, session_2
+
+    # r_auth_1 = requests.post("https://lightning.bitflyer.jp/",
+    #                          data=form_data,
+    #                          cookies={COOKIE_TOKEN: token_cookie},
+    #                          allow_redirects=False)
+
+    # assert r_auth_1.headers.get("Location") == "/Home/TwoFactorAuth"
+
+
+def main():
+    id = sys.argv[1]
+    password = sys.argv[2]
+    token_html, token_cookie = GetLoginTokens()
+    ASP_NET_SessionId, api_session_v2 = Login(id, password, token_html, token_cookie)
+    print(ASP_NET_SessionId, api_session_v2)
+
+
+if __name__ == "__main__":
+    main()
